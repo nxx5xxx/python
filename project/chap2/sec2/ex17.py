c86d77f8ece0c49b23e614f97c811f8abf84e684
nxx5xxx <dnwnx123@naver.com>
2023-03-23 10:05:34 +0900

    Print ,sep ,%s %d %f

---
+++ project/chap2/sec2/ex17.py
@@ -0,0 +1,16 @@
+#출력제어 : print , 이스케이프문자 , 형식제어자 , format
+name = "강아지"
+age = 10
+#sep
+print("이름",name,"\n나이",age,sep="=") #,사이에 =을 넣어줌
+#이스케이프 문자 : \n \t \f
+
+#형식지시자 : %s, %d, %f , ....
+print("이름: %s \n나이: %d" % (name, age))
+'''
+print를 printf처럼 사용이가능하다
+ex) print("name=%s age=%d" % (name,age))
+'''
+#format 함수
+print("당신의 이름은 {}이며, 나이는 {}세 입니다.".format(name, age))
+print("제가 가장 좋아하는 치킨은{1},{0},{2}입니다".format("후라이드","양념","간장"))
